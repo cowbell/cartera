7c705ce46e6dc46ae18726cb81e7877be11bbac9
Kuba Kuźma <kuba@jah.pl>
2015-03-21 17:33:53 +0100

    added firebase hosting

--- app/forms/item.js
+++ app/forms/item.js
@@ -1,6 +1,5 @@
 import Ember from "ember";
 import EmberValidations from "ember-validations";
-import App from "cartera/app";
 
 export default Ember.Object.extend(EmberValidations.Mixin, {
     validations: {
