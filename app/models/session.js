fad2c72a51120af8a2e4e6b83a65b883f3bc4038
Kuba Kuźma <kuba@jah.pl>
2015-05-26 21:16:13 +0200

    added helper methods

--- app/models/session.js
+++ app/models/session.js
@@ -5,5 +5,17 @@ export default Ember.Object.extend({
 
     uid: function () {
         return this.get("user").uid;
+    }.property("user"),
+
+    email: function () {
+        if (this.get("isAnonymous")) {
+            return "anonymous";
+        } else {
+            return this.get("user").password.email;
+        }
+    }.property("user", "isAnonymous"),
+
+    isAnonymous: function () {
+        return this.get("user").auth.provider === "anonymous";
     }.property("user")
 });
