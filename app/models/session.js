ddf40e075bc555313923885bb255c1acea66aaef
Kuba Kuźma <kuba@jah.pl>
2015-05-27 13:18:40 +0200

    added navbar

--- app/models/session.js
+++ app/models/session.js
@@ -9,11 +9,19 @@ export default Ember.Object.extend({
 
     email: function () {
         if (this.get("isAnonymous")) {
-            return "anonymous";
+            return undefined;
         } else {
             return this.get("user").password.email;
         }
     }.property("user", "isAnonymous"),
+
+    emailMD5: function () {
+        return md5(this.getWithDefault("email", ""));
+    }.property("email"),
+
+    gravatarURL: function () {
+        return "//www.gravatar.com/avatar/" + this.get("emailMD5") + "?d=mm";
+    }.property("emailMD5"),
 
     isAnonymous: function () {
         return this.get("user").auth.provider === "anonymous";
